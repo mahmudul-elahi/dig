85c99146a8a43d807fa4d3a10d758aeb97b8f6a1
Mahmudul Elahi <mahmudul.elahi@gmail.com>
2026-06-08 14:23:27 +0600

    update skills

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Dedoc\Scramble\Attributes\Endpoint;
+use Dedoc\Scramble\Attributes\Group;
+
+#[Group('Admin')]
 
 class DashboardController extends Controller
 {
-    public function index(): void
+
+    #[Endpoint(title: 'Dashboard', description: 'Retrieve dashboard information.')]
+    public function index()
     {
-        //
+        return auth()->user();
     }
 }
