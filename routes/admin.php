3029d4432b0caad328db3ad83e83ceadbdf47455
Mahmudul Elahi <mahmudul.elahi@gmail.com>
2026-06-08 13:14:07 +0600

    added: dashboard

--- routes/admin.php
+++ routes/admin.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 // Admin-only API routes (this file is included from routes/api.php inside the auth middleware group)
 Route::prefix('admin')->middleware('role:admin')->group(function () {
+    Route::get('dashboard', [DashboardController::class, 'index']);
     Route::get('users', [UserController::class, 'index']);
     Route::get('users/{user}', [UserController::class, 'show']);
     Route::patch('users/{user}', [UserController::class, 'update']);
